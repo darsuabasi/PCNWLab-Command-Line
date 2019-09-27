1206ce7180946d920f0c284942e774f40cf01584
darsuabasi <uduakabasiabasiurua@pursuit.org>
2019-09-26 22:17:52 -0400

    answersTohw

--- pset2.js
+++ pset2.js
@@ -22,6 +22,53 @@ In this problem, you will be downloading some files from online, then creating a
     app.js
     hello.html
 
+
+    cd dylanabasiurua
+    cd desktop
+    mkdir assets 
+    cd assets 
+    mkdir images
+    mkdir memes
+    mkdir src
+    cd .. till you get back to dylanabasiurua
+    cd Downloads
+    cd PCNWLab-Command-Line-master
+    cd stuff
+    git add amazon.jpg
+    git add facebook.png
+    git add google.jpg
+    git add meme.jpg
+    git add twitter.png
+    git add foo.js 
+    git add app.js
+    git add hello.html
+    git add amazon.jpg
+    mv amazon.jpg images 
+    mv facebook.png images
+    mv google.jpg images
+    mv twitter.png images 
+    mv meme.jpg memes
+    mv foo.js src
+    mv app.js pset2.js
+    mv hello.html pset2.js
+    
+    
+
+
+    mv amazon.jpg assets
+    mv facebook.png assets
+    mv google.jpg assets
+    mv twitter.png assets
+
+
+    cd Downloads
+    cd PCNWLab-Command-Line-master
+    git init
+    mkdir assets
+    git add pset2.js
+    
+    
+
 4. Open `hello.html` using a code editor and change "SOME_CLASS" to "Full Stack: Nights & Weekends". Save it.
 
 5. You should have the following commits:
